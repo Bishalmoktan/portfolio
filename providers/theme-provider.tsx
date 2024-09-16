fdb92b6a716e9a40aec73d7971f718a29bc1f49c
Bishal Moktan <bishalmoktan270@gmail.com>
2024-09-16 23:34:06 +0530

    Test: Checking the working of husky

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -6,4 +6,8 @@ import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+
+
+
+  
 }
